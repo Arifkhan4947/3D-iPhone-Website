c020ce49a309788b4ac7f650fbfb01a9f46d203e
Arif Khan <khanarif2738@gmail.com>
2024-10-28 10:24:45 +0530

    Created Light.jsx && IPhone.jsx in ModelView.jsx component where all the setup for 3D IPhone image.

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { PerspectiveCamera, View } from '@react-three/drei'
+import React, { Suspense } from 'react'
+import Light from './Light'
+import IPhone from './IPhone'
 
-const ModelView = () => {
+
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
   return (
-    <div>ModelView</div>
+    <View
+      index={index}
+      id={gsapType}
+      className={`border-2 border-red-500 w-full h-full ${index === 2} ? 'right-[-100%] : ''`}
+      >
+        {/* Ambient Light */}
+        <ambientLight intensity={0.3}/>
+        <PerspectiveCamera makeDefault position={[0, 0, 4]}/>
+        <Light/>
+
+        <Suspense fallback={<div>Loading</div>}>
+          <IPhone/>
+        </Suspense>
+    </View>
   )
 }
 
