c38c6f2a89c4364a96d649b070274fb264c4f182
Arif Khan <khanarif2738@gmail.com>
2024-10-29 10:41:00 +0530

    Create Features.jsx component and Add heading and apply gsap animatio of this text.

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,18 @@ import Highlights from './components/Highlights'
 import Model from './components/Model'
 
 import * as Sentry from '@sentry/react';
+import Features from './components/Features'
 
 const App = () => {
 
 
   return (
     <main className='bg-black'>
-      <Navbar/>
-      <Hero/>
-      <Highlights/>
-      <Model/>
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
     </main>
   )
 }
